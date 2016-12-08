232a6191cf8806dff16954ec10eaeac5d3b80132
binyuea <binyuea@gmail.com>
2016-12-08 15:31:47 +0000

    post update

---
+++ _site/Download.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import urllib
+import re
+from bs4 import BeautifulSoup
+from time import gmtime, strftime
+
+url = 'http://mp.weixin.qq.com/s?__biz=MzA3MDM3MDYwMg==&mid=2653888920&idx=1&sn=006bf0be798e6441c4cb498a1251d45b&chksm=84e655ddb391dccb9e47cbe66b644f650908316fa5c94b4cf22172b68879b469443d9d20835b&mpshare=1&scene=1&srcid=1208LrYnsk6zf70iq0A6rbMp'
+# url = 'http://mp.weixin.qq.com/s?__biz=MzA3MDM3MDYwMg==&mid=2653888920&idx=6&sn=ce64610af0a495c1159af89c1ec200ea&chksm=84e655ddb391dccb2918ba067fd05fab6a10ce7734167afda6d1eb7ee6959614637f155f6957&mpshare=1&scene=1&srcid=1208Y06Ofu0jGs5ej7hStjsn#rd'
+out_dir = '_posts/'
+
+
+def getDescription(soup):
+	results = soup.find_all("span")
+	for item in results:
+		if item.has_attr("class"):
+			continue
+		if item.span:
+			continue
+		if item.getText() and item.getText() != ' ':
+			return item.getText()	
+
+
+def download(url):
+	page = urllib.urlopen(url).read()
+	soup = BeautifulSoup(str(page),'html.parser')
+
+	# writeHead
+	Title = soup.find('title').getText()
+	# if ' ' in Title:
+	# 	Title = Title.split(' ')
+	# 	Title = ''.join(Title)
+	if '\'' in Title:
+		Title = Title.split('\'')[1]
+		# Title = '~'.join(Title)
+	if '\"' in Title:
+		Title = Title.split('\"')[1]
+		# Title = '~'.join(Title)
+	Title = Title.encode('utf-8')
+	Time = strftime("%Y-%m-%d-", gmtime())
+	f = open('_posts/'+ Time + Title + '.md','w+')
+	f.write('---\n')
+	f.write('layout: post \n')
+	f.write('title: '+ Title +'\n')
+	f.write('description: '+ getDescription(soup).encode('utf-8') + '\n')
+	f.write('category: Moments \n')
+	f.write('keywords: Moments, Weixin \n')
+	f.write('comments: true \n')
+	f.write('---\n')
+	# f.write()
+
+	flag = False
+	results = soup.find_all("span")
+	for item in results:
+		if item.has_attr("class"):
+			continue
+		if item.span:
+			continue
+		if item.getText() and item.getText() != ' ':
+			if flag:
+				# print item.getText(),
+				f.write(item.getText().encode('utf-8'))
+			else:
+				flag = True
+
+
+	f.close()
+
+
+# def writeHead(title):
+
+
+if __name__ == '__main__':
+	download(url)
